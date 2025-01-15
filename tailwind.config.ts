7b6b8c324786f9636d34fa5f655f4d0bfd5c9969
ynncstslv <ynncstslv@gmail.com>
2025-01-15 17:06:15 -0300

    feat: adding some custom screen sizes and container to tailwind config

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,18 +1,31 @@
-import type { Config } from "tailwindcss";
+import type { Config } from 'tailwindcss';
 
 export default {
-  content: [
-    "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
-    "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
-    "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
-  ],
-  theme: {
-    extend: {
-      colors: {
-        background: "var(--background)",
-        foreground: "var(--foreground)",
-      },
-    },
-  },
-  plugins: [],
+	content: [
+		'./src/pages/**/*.{js,ts,jsx,tsx,mdx}',
+		'./src/sections/**/*.{js,ts,jsx,tsx,mdx}',
+		'./src/components/**/*.{js,ts,jsx,tsx,mdx}',
+		'./src/app/**/*.{js,ts,jsx,tsx,mdx}',
+	],
+	theme: {
+		screens: {
+			sm: '375px',
+			md: '768px',
+			lg: '1200px',
+		},
+		container: {
+			center: true,
+			padding: {
+				DEFAULT: '1rem',
+				md: '2rem',
+			},
+		},
+		extend: {
+			colors: {
+				background: 'var(--background)',
+				foreground: 'var(--foreground)',
+			},
+		},
+	},
+	plugins: [],
 } satisfies Config;
