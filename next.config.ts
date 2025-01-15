6401b095b00b4d541cc456dd78689526759acf6d
ynncstslv <ynncstslv@gmail.com>
2025-01-15 17:21:14 -0300

    feat: add custom snippet to deal with svg images in next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,45 @@
-import type { NextConfig } from "next";
+import type { NextConfig } from 'next';
 
 const nextConfig: NextConfig = {
-  /* config options here */
+	webpack(config) {
+		const fileLoaderRule = config.module.rules.find((rule: { test?: RegExp }) =>
+			rule.test?.test?.('.svg')
+		);
+
+		config.module.rules.push(
+			{
+				...fileLoaderRule,
+				test: /\.svg$/i,
+				resourceQuery: /url/, // *.svg?url
+			},
+			{
+				test: /\.svg$/i,
+				issuer: fileLoaderRule.issuer,
+				resourceQuery: { not: [...fileLoaderRule.resourceQuery.not, /url/] }, // exclude if *.svg?url
+				use: {
+					loader: '@svgr/webpack',
+					options: {
+						svgoConfig: {
+							plugins: [
+								{
+									name: 'preset-default',
+									params: {
+										overrides: {
+											removeViewBox: false,
+										},
+									},
+								},
+							],
+						},
+					},
+				},
+			}
+		);
+
+		fileLoaderRule.exclude = /\.svg$/i;
+
+		return config;
+	},
 };
 
 export default nextConfig;
