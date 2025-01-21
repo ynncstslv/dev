8cf02bd8f5cb292551dd6430932efe8c1254fe07
ynncstslv <ynncstslv@gmail.com>
2025-01-21 12:00:13 -0300

    fix: opacity on footer section

--- src/sections/Footer.tsx
+++ src/sections/Footer.tsx
@@ -4,7 +4,7 @@ import ArrowUpRightIcon from '@/assets/icons/arrow-up-right.svg';
 export default function Footer() {
 	return (
 		<footer className="relative z-0 overflow-x-clip">
-			<div className="w-[1600px] h-[400px] bottom-0 left-1/2 absolute -translate-x-1/2 bg-[#BD93F9]/25 -z-50 [mask-image:radial-gradient(50%_50%_at_bottom_center,black,transparent)]" />
+			<div className="w-[1600px] h-[400px] bottom-0 left-1/2 absolute -translate-x-1/2 bg-[#BD93F9]/15 -z-50 [mask-image:radial-gradient(50%_50%_at_bottom_center,black,transparent)]" />
 			<div className="container">
 				<div className="flex flex-col items-center gap-6 py-6 text-sm border-t border-[#F8F8F2]/15 md:flex-row md:justify-between">
 					<div className="text-[#F8F8F2]/40">
